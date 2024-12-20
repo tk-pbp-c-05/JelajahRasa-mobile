2ddc6e9097b79d605285ffdb33693ea6f97b2482
kaylagrata <kayla.a.budiawan@gmail.com>
2024-12-20 15:49:21 +0700

    remove navigation error (back icon) on show_favorite

--- lib/favorite/screens/show_favorite.dart
+++ lib/favorite/screens/show_favorite.dart
@@ -61,12 +61,6 @@ class _ShowFavoritePageState extends State<ShowFavorite> {
       appBar: AppBar(
         backgroundColor: Colors.white,
         elevation: 0,
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back, color: Colors.black),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
         title: const Text(
           "Favourite Dishes",
           style: TextStyle(
